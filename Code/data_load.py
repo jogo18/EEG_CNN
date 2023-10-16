2814ab07287d8891cd49e8b4cf0035f1ac2e55e8
Jakob Olesen <jagrole@gmail.com>
2023-10-16 15:10:46 +0200

    removed stuff

--- Code/data_load.py
+++ Code/data_load.py
@@ -1,19 +1,11 @@
 import scipy.io as sio
 import numpy as np
-# ID = '0001'
-# dB = '3'
-# onoff = 'ON'¨
-#char(ic_clean.trialinfo(1,18:end))
 def data_loader(ID, dB, onoff, index):
     path = "C:/Users/jakob/Documents/AAU/7_semester/Projekt/DATA"
     file = f"{path}/{onoff}_{dB}/ID{ID}_preprocesseddata_{dB}dB{onoff}"
     data = sio.loadmat(file)
     temp_trial_data = data['ic_clean']['trial']
-    # temp_labels = data['ic_clean']['label']
-    # labels = np.concatenate(np.concatenate(np.concatenate(np.concatenate(temp_labels))))
     temptimes = data['ic_clean']['time'][0]
-    # tempunmixingmatrix = data['ic_clean']['icaunmixingmatrix']
-    # unmix_matr = np.concatenate(np.concatenate(np.concatenate(np.concatenate(tempunmixingmatrix))))
     times = np.concatenate(np.concatenate(np.concatenate(np.concatenate(temptimes))))
     conc_trial_data = np.concatenate(np.concatenate(np.concatenate(np.concatenate(temp_trial_data))))
     temptrialinfo = data['ic_clean']['trialinfo']
